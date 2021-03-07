201b33369f062359c81733256e8657b6f7102056
Kumar Kote <skote@salesforce.com>
2021-03-07 18:35:08 -0500

    adding contact commit

--- routes/contact.js
+++ routes/contact.js
@@ -2,16 +2,16 @@ var express = require('express');
 var _ = require("lodash");
 var router = express.Router();
 
-app.post("/contact", function (req, res) {
+app.post('/', function (req, res) {
     // do something with req.body
     console.log("http post /contact call. processing..");
     res.status(201).end();
 }); 
-app.get("/contact", function (req, res) {
-    // do something with req.body
-    console.log("HTTP get on /contact. processing..");
-    res.status(201).end();
-}); 
+// app.get("/contact", function (req, res) {
+//     // do something with req.body
+//     console.log("HTTP get on /contact. processing..");
+//     res.status(201).end();
+// }); 
 
 
 module.exports = router;
